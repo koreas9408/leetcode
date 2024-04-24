82b04ffeca898a6bcddc4c0f0a86d8deae66588d
임승현 <koreas9408@gmail.com>
2024-04-24 23:40:04 +0900

    Time: 10 ms (87.68%), Space: 60.2 MB (31.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,6 +1,9 @@
+import java.util.Set;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
+        
         for (int num : nums) {
             if (set.contains(num)) return true;
             set.add(num);
